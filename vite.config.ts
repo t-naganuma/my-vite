6835639a457a6a1d6271cc0b49d5f7ad4297edc6
t-naganuma <takuya.naganuma@gmail.com>
2022-10-04 21:26:33 +0900

    config update

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@/": path.join(__dirname, "./src/"),
+    },
+  },
   css: {
     postcss: {
-      plugins: [require("autoprefixer")],
+      plugins: [autoprefixer],
     },
+  },
+  server: {
+    port: 8080,
+    open: true,
+    origin: "http://127.0.0.1:8080",
   },
 });
